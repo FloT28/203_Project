dda7f4aee00d0bb84276cebc3dddafd403c3d098
FloT28 <Florence.Taele@gmail.com>
2025-06-05 00:27:18 +1200

    python feature 1 (half completed~still working on feature) (draft (trial underway))

--- Shoez_express.py
+++ Shoez_express.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template, redirect, url_for, request, flash
+from flask import Flask, render_template, redirect, url_for, request, flash, session
 from werkzeug.utils import secure_filename
 import os #libary used to import the Operating system - Upload image file for user
-
+from flask_session import Session #
 
 app = Flask(__name__)
+app.secret_key = "ShoezExpress"
 
 upload_folder = os.path.join('static', 'upload')
 allowed_extensions = {'png','jpg','jpeg','gif'}
@@ -15,32 +16,54 @@ app.config['UPLOAD'] = upload_folder
 def layout(): 
     return render_template('Layout.html')
 
+#user1.html - new file to store and display user information
+
+#database trial only! - connection with real database coming soon!
+database = {
+        'Florence@gmail.com': 'flowers',
+        'Lily@gmail.com': 'flow',
+        'Laura@gmail.com': 'abc',
+        }
+
 #Sign-in route page
-@app.route('/sign-in/',methods=('GET','POST'))
+@app.route('/sign-in',methods=('GET','POST'))
 def sign_in(): 
-    
-    return render_template('sign-in2.html')
+    eml = request.form['Email']
+    pwrd = request.form['Password']
+    if pwrd not in database: 
+        return render_template('sign-in 1 1.html', message = 'invalid')
+    else: 
+        if database[eml] != pwrd:
+            return render_template('sign-in 1 1.html', message = "invalid password")
+        else: 
+            return render_template('sign-up 1 1.html')
 
 #Sign-Up route page
-@app.route('/sign-up/',methods=('GET','POST'))
+@app.route('/sign-up/', methods=('GET','POST'))
 def sign_up():
-    return render_template('sign-up2.html')
+    return render_template('sign-up 1 1.html')
 
 #Profile route page
 @app.route('/profile_1', methods=['GET','POST'])
 def profile(): 
     if request.method == 'POST':
-        file = request.files['img']
+        file = request.files['img']#type of file requesting
         filename = secure_filename(file.filename)
         file.save(os.path.join(app.config['UPLOAD'], filename))
         img = os.path.join(app.config['UPLOAD'], filename)
-        return render_template('profile 3.html', img=img)
+        return render_template('profile 3.html', img=img)#return to the original html page (profile)
     return render_template('profile 3.html')
 
 # Cart route page
 @app.route('/Cart/')
 def cart(): 
     return render_template('Cart1.html')
+
+# Cart route page
+@app.route('/cart1')
+def cart1(): 
+    return render_template('test.html')
+
 
 
 if __name__ == '__main__':
