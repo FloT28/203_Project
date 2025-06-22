47ef5981146e3a2fa9b0cba10244ca4e3e679ed4
FloT28 <Florence.Taele@gmail.com>
2025-06-22 18:50:35 +1200

    completed files, all working

--- Shoez_express.py
+++ Shoez_express.py
@@ -13,13 +13,23 @@ allowed_extensions = {'png','jpg','jpeg','gif'}
 
 app.config['UPLOAD'] = upload_folder
 
-#Database connection (2) - Users (sign-up * registered users)
+#Database connection (2) - Users (registered users)
 def db_connection(): 
     conn = psycopg2.connect(host='localchost',
                             databse='users',
                             user=os.environ['DB_USERNAME'],
-                            password=os.environ['DB_PASSWORD'])
+                            password=os.environ['DB_PASSWORD']
+                            )
     return conn
+
+
+# Connect to the database
+conn1 = psycopg2.connect(dbname="users", user="postgres",
+                        password="flowers7*", host="localhost", port="5432")
+
+# create a cursor
+cur1 = conn1.cursor()
+
 
 #Database connection (2) - Contact Page
 def database_connect(): 
@@ -29,25 +39,33 @@ def database_connect():
                             password=os.environ['DB_PASSWORD'])
     return conn 
 
-# Connect to the database
-conn = psycopg2.connect(database="users", user="postgres",
-                        password="flowers7*", host="localhost", port="5432")
-
-# create a cursor
-cur = conn.cursor()
-
 # Connect to the database 2 
-conn = psycopg2.connect(database="contact", user="postgres",
+conn2 = psycopg2.connect(database="contact", user="postgres",
                         password="flowers7*", host="localhost",port="5432")
 
 #cursor created 
-cur = conn.cursor()
+cur2 = conn2.cursor()
+
+#TABLE CREATED - users database 
+@app.route('/db2')
+def db2(): 
+    try:
+        #postgreSQL database connection
+        conn = psycopg2.connect(dbname="users", #database name
+                                user="postgres",#database user
+                                password="flowers7*",
+                                host="localhost",
+                                port="5432")
+        print("Succeffully connected to PostgreSQL database!")
+    except psycopg2.Error as e: 
+        print(f"Error in connecting with database: {e}")
+        exit()
 
 #TABLE CREATION - contact
 @app.route('/db')
 def db(): 
     try: 
-        cur = conn.cursor() 
+        cur = conn2.cursor() 
         cur.execute('''
                 CREATE TABLE IF NOT EXISTS contact (
                     id SERIAL PRIMARY KEY,
@@ -55,103 +73,93 @@ def db():
                     email TEXT,
                     message TEXT,)
                 ''')
-        conn.commit() 
+        conn2.commit() 
         return "table created successfully!"
     except Exception as e: 
-        conn.rollback() 
+        conn2.rollback() 
         return f"Error: {e}"
 
-#TABLE CREATED - users database 
-@app.route('/db2')
-def db2(): 
-    try:
-        cur = conn.cursor() 
-        cur.execute('''
-                CREATE TABLE IF NOT EXISTS customers (
-                    id SERIAL PRIMARY KEY,
-                    name text NOT NULL,
-                    email text NOT NULL,
-                    password text NOT NULL)
-                ''')
-        conn.commit() 
-        return "table created successfully!"
-    except Exception as e: 
-        conn.rollback() 
-        return f"Error: {e}"
-
-
+#home page
 @app.route('/')
-def cart(): 
-    return render_template('Cart.html')
+def layout(): 
+    return render_template('layout.html')
 
+#contact page
 @app.route('/contact_us', methods=['GET','POST'])
 def contact_us(): 
+    #collect user input from form 
     if request.method == "POST": 
         name = request.form.get('name')
         email = request.form.get('email')
         message = request.form.get('message')
 
         #insert data into postgreSQL database
-        cur.execute('INSERT INTO contact (name, email, message) ' 
+        cur2.execute('INSERT INTO contact (name, email, message) ' 
                     'VALUES(%s, %s, %s)',
                     (name, email, message)
         )
 
-            #commit changes
-        conn.commit()
-        return redirect(url_for('contact_us', name=name, email=email, message=message))
+        #commit changes to database
+        conn2.commit()
+        #redirect user to another page
+        return redirect(url_for('confirm_mess', name=name, email=email, message=message))
         
-    cur.execute("SELECT * FROM contact")
-    print(cur.fetchall())
+    cur2.execute("SELECT * FROM contact")
+    print(cur2.fetchall())
 
     return render_template('contact_us.html')
 
 #display name and message! 
 @app.route('/confirm_mess')
 def confirm_mess(): 
-    cur = conn.cursor()
+    cur = conn2.cursor()
     cur.execute("SELECT * FROM contact")
     rows = cur.fetchall()
     return render_template('Confirm_mess.html', contact=rows)
 
-
 #Sign-Up route page
 @app.route('/sign_up', methods=['GET','POST'])
 def sign_up():
-    if request.method == "POST": 
-        name = request.form.get('name')
+    #Collect user input from form 
+    if request.method == 'POST': 
+        username = request.form.get('username')
         email = request.form.get('email')
         password = request.form.get('password')
 
-        if not name or not email or not password:
-            return "All fields are required", 400
+        print(f"Username: {username}, Email: {email}, Password: {password}")
 
         #insert data into postgreSQL database
-        cur.execute('INSERT INTO users (name, email, password) ' 
+        cur1.execute('INSERT INTO users (username, email, password) ' 
                     'VALUES(%s, %s, %s)',
-                    (name, email, password)
+                    (username, email, password)
         )
-            #commit changes
-        conn.commit()
-        return redirect(url_for('profile1', name=name))
-    
-    cur.execute("SELECT * FROM customers")
-    print(cur.fetchall())
+        
+        #commit changes
+        conn1.commit()
+        #redirect user to profile page 
+        return redirect(url_for('profile1', username=username))
+
+    cur1.execute("SELECT * FROM users")
+    print(cur1.fetchall())
 
     return render_template('sign-up 1_1.html')
 
+#Registered Users
+@app.route('/Users')
+def Users():
+    cur = conn1.cursor()
+    cur.execute("SELECT * FROM users")
+    rows = cur.fetchall()
+    return render_template("Users.html", users=rows)
+
+#Future Implementation!
 #Sign-in route page
 @app.route('/sign_in',methods=['GET','POST'])
 def sign_in(): 
     email = request.form.get('email')
     password = request.form.get('password')
 
-    if email not in Users: 
-        return render_template('sign-in 1 1.html', message = 'invalid email')
-    elif password not in Users: 
-        return render_template('sign-in 1 1.html', message = 'invalid password')
-    else: 
-        return render_template('profile 3.html')
+    return render_template('sign-in 1 1.html')
 
 #Profile route page
 @app.route('/profile_1', methods=['GET','POST'])
@@ -171,6 +179,8 @@ def profile():
 def profile1(): 
     #collect user input (username)
     username = request.args.get('username')
+
+
     #redirect user to original main profile page (have user links in a squared section)
     return render_template('profile 3.html', username=username)
 
@@ -179,18 +189,9 @@ def profile1():
 def profile_settings(): 
     return render_template('profile_settings.html')
 
-#Registered Users
-@app.route('/Users')
-def Users():
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM users")
-    rows = cur.fetchall()
-    return render_template("Users.html", users=rows)
-
-
 # Cart route page
-@app.route('/Cart/')
-def Cart(): 
+@app.route('/cart')
+def cart(): 
     return render_template('Cart.html')
 
 #Payment page
