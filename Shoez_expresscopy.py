d4f92c466638b62625423a5df74d1adc05537d6c
FloT28 <Florence.Taele@gmail.com>
2025-07-01 14:03:05 +1200

    updated code (work-in-progress for cart page and sign-up/sign-in)

--- Shoez_expresscopy.py
+++ Shoez_expresscopy.py
@@ -5,12 +5,7 @@ import MySQLdb.cursors
 import re
 import os #libary used to import the Operating system - Upload image file for user
 import psycopg2#database connection - postgreSQL
-import pymysql#library to allow the usage of mysql 
-
-#ERROR HERE 
-from db_config import mysql #connection between db_config and msql
-#ERROR HERE ^ 
-
+import pymysql #library to allow the usage of mysql-
 #from werkzeug import generate_password_hash, check_password_hash
 from werkzeug.security import generate_password_hash, check_password_hash
 
@@ -24,13 +19,22 @@ allowed_extensions = {'png','jpg','jpeg','gif'}
 
 app.config['UPLOAD'] = upload_folder
 
-#MySQL Configurations
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'Ilikeflowers7*'  # Enter your MySql password 
-app.config['MYSQL_DB'] = 'accounts'
+mysql1 = MySQL()
+mysql2 = MySQL() 
 
-mysql = MySQL(app)
+#MySQL Configurations - Database 1 (products)
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'Ilikeflowers7*'
+app.config['MYSQL_DATABASE_DB'] = 'products'
+app.config['MYSQL_DATABSE_HOST'] = 'localhost'
+mysql1.init_app(app)
+
+#MySQL Configurations - Database 2 (User sign-in/sign-up)
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'Ilikeflowers7*'
+app.config['MYSQL_DATABASE_DB'] = 'accounts'
+app.config['MYSQL_DATABSE_HOST'] = 'localhost'
+mysql2.init_app(app)
 
 #home page
 @app.route('/')
@@ -46,9 +50,9 @@ def add_product_to_cart():
         _code = request.form['code']#code for product 
         #validation for the received values 
         if _quantity and _code and request.method == 'POST': 
-            conn = mysql.connect() 
-            cursor = conn.cursor(pymsql.cursors.DictCursor)
-            cursor.execute("SELECT * FROM product WHERE code=%s, _code")
+            conn = mysql1.connect() 
+            cursor = conn.cursor(pymysql.cursors.DictCursor)
+            cursor.execute("SELECT * FROM product WHERE code=%s", _code)
             row = cursor.fetchone() 
 
             itemArray = { row['code'] : {'name' : row['name'], 'code' : row['code'], 'quantity' : _quantity, 'price' : row['price'], 'image' : row['image'], 'total_price':_quantity * row['price']}}
@@ -73,14 +77,14 @@ def add_product_to_cart():
                     session['cart_item'] = array_merge(session['cart_item'], itemArray)
                     
                 for key, value in session['cart_item'].items(): 
-                    individual_quantity = int(session['cart_item'][key]['quantity'])
-                    individual_price = float(session['cart_item'][key]['total_price'])
-                    all_total_quantity = all_total_quantity + individual_quantity 
-                    all_total_price = all_total_price + individual_price 
+                        individual_quantity = int(session['cart_item'][key]['quantity'])
+                        individual_price = float(session['cart_item'][key]['total_price'])
+                        all_total_quantity = all_total_quantity + individual_quantity 
+                        all_total_price = all_total_price + individual_price 
             else: 
                 session['cart_item'] = itemArray 
-            all_total_quantity - all_total_quantity + _quantity
-            all_total_price = all_total_price + _quantity * row['price']
+                all_total_quantity = all_total_quantity + _quantity
+                all_total_price = all_total_price + _quantity * row['price']
 
             session['all_total_quantity'] = all_total_quantity 
             session['all_total_price'] = all_total_price
@@ -91,8 +95,22 @@ def add_product_to_cart():
     except Exception as e: 
         print(e)
     finally: 
-        cursor.close()
-        conn.close() 
+            cursor.close()
+            conn.close() 
+
+@app.route('/products1')
+def products1(): 
+    try: 
+            conn = mysql1.connect() 
+            cursor = conn.cursor(pymysql.cursors.DictCursor)
+            cursor.execute("SELECT * FROM products")
+            rows = cursor.fetchall()
+            return render_template('products1.html',products=rows)
+    except Exception as e: 
+        print(e)
+    finally: 
+            cursor.close() 
+            conn.close() 
 
 #empty cart page 
 @app.route('/empty')
@@ -107,128 +125,77 @@ def empty_cart():
 @app.route('/delete/<string:code>')
 def delete_product(code): 
     try: 
-        all_total_price = 0
-        all_total_quantity = 0 
-        session.modified = True 
+            all_total_price = 0
+            all_total_quantity = 0 
+            session.modified = True 
 
-        for item in session['cart_item'].items(): 
-            if item[0] == code: 
-                session['cart_item'].pop(item[0], None)
-                if 'cart_itm' in session: 
-                    for key, value in session['cart_item'].items(): 
-                        individual_quantity = int(session['cart_item'][key]['quantity'])
-                        individual_price = float(session['cart_item'][key]['total_price'])
-                        all_total_quantity = all_total_quantity + individual_quantity 
-                        all_total_pric = all_total_price + individual_price 
-                break
+            for item in session['cart_item'].items(): 
+                if item[0] == code: 
+                    session['cart_item'].pop(item[0], None)
+                    if 'cart_itm' in session: 
+                        for key, value in session['cart_item'].items(): 
+                            individual_quantity = int(session['cart_item'][key]['quantity'])
+                            individual_price = float(session['cart_item'][key]['total_price'])
+                            all_total_quantity = all_total_quantity + individual_quantity 
+                            all_total_price = all_total_price + individual_price 
+                    break
 
-        if all_total_quantity == 0: 
-            session.clear() 
-        else: 
-            session['all_total_quantity'] = all_total_quantity 
-            session['all_total_price'] = all_total_price 
+            if all_total_quantity == 0: 
+                session.clear() 
+            else: 
+                session['all_total_quantity'] = all_total_quantity 
+                session['all_total_price'] = all_total_price 
 
-        return redirect(url_for('.products'))
+            return redirect(url_for('.products'))
     except Exception as e: 
         print(e)
 
 def array_merge( first_array, second_array ):
-    if isintance( first_array, list ) and isintance( second_array, list ):
+    if isinstance( first_array , list ) and isinstance(second_array , list):
         return first_array + second_array
-    elif isintance( first_array, dict) and isintance( second_array, dict ):
-        return dict( list( first_array.items()) + list( second_array.items() ) )
-    elif isinstance( first_array, set ) and isintance( second_array, set ):
-        return first_array.union ( second_array )
-        return False 
+    elif isinstance( first_array, dict) and isinstance( second_array, dict):
+        return dict( list( first_array.items() ) + list( second_array.items()))
+    elif isinstance( first_array , set ) and isinstance( second_array, set ):
+        return first_array.union( second_array )
+    return False 
 
 #contact page
 @app.route('/contact_us', methods=['GET','POST'])
 def contact_us(): 
-    #collect user input from form 
-    if request.method == "POST": 
-        name = request.form.get('name')
-        email = request.form.get('email')
-        message = request.form.get('message')
-
-        #insert data into postgreSQL database
-        cur2.execute('INSERT INTO contact (name, email, message) ' 
-                    'VALUES(%s, %s, %s)',
-                    (name, email, message)
-        )
-
-        #commit changes to database
-        conn2.commit()
-        #redirect user to another page
-        return redirect(url_for('confirm_mess', name=name, email=email, message=message))
-        
-    cur2.execute("SELECT * FROM contact")
-    print(cur2.fetchall())
-
     return render_template('contact_us.html')
 
 #display name and message! 
 @app.route('/confirm_mess')
 def confirm_mess(): 
-    cur = conn2.cursor()
-    cur.execute("SELECT * FROM contact")
-    rows = cur.fetchall()
-    return render_template('Confirm_mess.html', contact=rows)
 
-#Sign-Up route page
-@app.route('/sign_up', methods=['GET','POST'])
-def sign_up():
-    #Collect user input from form 
-    if request.method == 'POST': 
-        username = request.form.get('username')
-        email = request.form.get('email')
-        password = request.form.get('password')
-
-        print(f"Username: {username}, Email: {email}, Password: {password}")
-
-        #insert data into postgreSQL database
-        cur1.execute('INSERT INTO users (username, email, password) ' 
-                    'VALUES(%s, %s, %s)',
-                    (username, email, password)
-        )
-        
-        #commit changes
-        conn1.commit()
-        #redirect user to profile page 
-        return redirect(url_for('profile1', username=username))
-
-    cur1.execute("SELECT * FROM users")
-    print(cur1.fetchall())
-
-    return render_template('sign-up 1_1.html')
+    return render_template('Confirm_mess.html')
 
 #Registered Users
 @app.route('/Users')
 def Users():
-    cur = conn1.cursor()
-    cur.execute("SELECT * FROM users")
-    rows = cur.fetchall()
-    return render_template("Users.html", users=rows)
+
+    return render_template("Users.html")
 
 #Future Implementation!
 #Sign-in route page
-@app.route('/sign_in',methods=['GET','POST'])
+@app.route('/sign_in', methods=['GET','POST'])
 def sign_in(): 
     msg=''#default message variable to use
     if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
         username = request.form['username']
         password = request.form['password']
-        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor = mysql2.connection.cursor(MySQLdb.cursors.DictCursor)
         cursor.execute('SELECT * FROM accounts WHERE username = %s AND password = %s',(username, password))
         account = cursor.fetchone()
         if account: 
             session['loggedin'] = True
             session['id'] = account['id']
             session['username'] = account['username']
-            return render_Template('profile3.html',msg='Logged in Successfully!')
+            return render_template('profile3.html',msg='Logged in Successfully!')
         else:
             msg='Incorrect username/password!'
-    return render_template('sign-in 1 1.html')
-
+            return render_template('sign-in 1 1.html')
+#user can sign-out of their account
 @app.route('/sign-out')
 def logout(): 
     session.pop('logged_in', None)
@@ -236,15 +203,34 @@ def logout():
     session.pop('username', None)
     return redirect(url_for('sign_in'))
 
-@app.route('/sign_up',methods=['GET','POST'])
+#sign-up route for users to register with Shoez-express
+@app.route('/sign_up', methods=['GET','POST'])
 def sign_up():
-    msg = ''
+    msg = ''#default variable message intialization * used to store messages
     if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form:
-        username = request.form['username']
-        password = request.form['password']
-        email = request.form['email']
-        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute('SELECT * FROM accounts WHERE ')
+        username = request.form['username']#collecting username from user(form)
+        password = request.form['password']#collecting password from user(form)
+        email = request.form['email']#collecting email from user(form)
+        cursor = mysql2.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM accounts WHERE username = %s',(username,))
+        account = cursor.fetchone()
+        if account: 
+            msg = 'Account already exists!'
+        elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+            msg = 'Invalid email address!'
+        elif not re.match(r'[A-Za-z0-9]+', username):
+            msg = 'Username must contain only letters and numbers!'
+        elif not username or not password or not email: 
+            msg = 'Please fill out the form!'
+        else: 
+            cursor.execute('INSERT INTO accounts VALUES (NULL, %s, %s, %s)',(username, password, email))
+            mysql2.connection.commit()
+            #connect with mysql database 
+            msg = 'You have successfully registered!'
+        return render_template('sign-up 1_1.html', msg=msg)
+    
+    
+
 #Profile route page
 @app.route('/profile_1', methods=['GET','POST'])
 def profile(): 
